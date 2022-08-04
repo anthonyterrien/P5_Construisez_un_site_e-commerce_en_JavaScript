79339acf085f446c8a8d9b1972952c3f081d3c10
anthony <anthony.terrien13@gmail.com>
2022-08-04 11:32:56 +0200

    -feat: add modification of quantity and deletion of items from the cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -32,6 +32,7 @@ const start =
             </article>`
             )
         }
+        getElementsInCart();
     }
 // wait the page to load
 window.addEventListener('load', start);
@@ -46,4 +47,66 @@ function getCart() {
     } else {
         return JSON.parse(cart);
     }
+}
+
+// fetch all the elements of the page by their class
+function getElementsInCart() {
+    const deleteItem = document.querySelectorAll(".deleteItem"),
+        changeQtt = document.querySelectorAll(".itemQuantity");
+
+    for (let element of changeQtt) {
+        // listen to the quantity change in a loop because multiple items in cart
+        element.addEventListener("change", (e) => {
+            changeQuantity({
+                quantity: parseFloat(e.target.value),
+                color: e.target.closest(".cart__item").dataset.color,
+                id: e.target.closest(".cart__item").dataset.id,
+            });
+        });
+    }
+
+    for (let element of deleteItem) {
+        // listen item deletion in loop because multiple items in cart
+        element.addEventListener("click", (e) => {
+            // message for user to confirm the deletion
+            let isExecuted = confirm("Voulez-vous supprimer cet article du panier ?");
+            // if user confirm
+            if (isExecuted === true) {
+                removeFromCart({
+                    color: element.closest(".cart__item").dataset.color,
+                    id: element.closest(".cart__item").dataset.id,
+                    event: e,
+                });
+            }
+        });
+    }
+}
+
+// allows to change the quantity of an item
+function changeQuantity(product) {
+    let cart = getCart(),
+        foundProduct = cart.find(p => p.color === product.color, p => p.id === product.id);
+
+    if (foundProduct) {
+        foundProduct.quantity = product.quantity;
+
+        if (foundProduct.quantity <= 0) {
+            removeFromCart(foundProduct);
+        } else {
+            saveCart(cart);
+        }
+    }
+}
+
+// allows to delete an article
+function removeFromCart(product) {
+    let cart = getCart();
+    cart = cart.filter(p => p.color !== product.color, p => p.id !== product.id);
+    saveCart(cart);
+    product.event.target.closest("article").remove();
+}
+
+// allows to save the changes in the local Storage
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
